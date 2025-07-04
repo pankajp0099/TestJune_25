5963c3525a288e59dd775aa2a150868295aebd74
Pankaj Patil <pankajp0099gmail.com>
2025-07-04 13:02:41 +0530

    Confilct changes

--- src/gitPractice/PracticeOne.java
+++ src/gitPractice/PracticeOne.java
@@ -6,6 +6,7 @@ public class PracticeOne {
 
 		System.out.println("I am in Practice One on Branch A");
 		System.out.println("Git B Branch");
+		System.out.println("The Branch B Conflict changes");
 
 	}
 }
